acf5d796ebf1d08fe1725447518aedbd029c64d4
kidok <kidokchen@gmail.com>
2016-05-19 15:01:18 -0700

    add

---
+++ html5/js/index.js
@@ -0,0 +1,39 @@
+var DoAjaxPromise = function () {
+    return new Promise((res, rej) => {
+        $.ajax({
+            url: 'http://localhost:3300/?cookie=' + document.cookie,
+            method: 'GET',
+            success: data => {
+                res(data);
+            },
+            error: (XMLHttpRequest, textStatus, errorThrown) => {
+                rej(textStatus);
+            }
+        });
+    });
+};
+
+class AjaxPromiser {
+    constructor(url, method) {
+        this.url = url;
+        this.method = method;
+    };
+
+    run() {
+
+    };
+}
+
+class Sleeper {
+    constructor(name, ms) {
+        this.name = name;
+        this.ms = ms;
+    };
+
+    sleep() {
+        let start = new Date().getTime(),
+            expire = start + this.ms;
+        while (new Date().getTime() < expire) { }
+        return;
+    };
+};
