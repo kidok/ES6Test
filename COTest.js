79a17821615810c338e148f9eb828ba39ed47a9a
kidok <kidokchen@gmail.com>
2016-05-19 17:25:28 -0700

    add

--- COTest.js
+++ COTest.js
@@ -21,26 +21,29 @@ fn('test2').then(function (v) {
 //   var result = yield Promise.resolve(true);
 // }).catch(onerror);
 
-// co(function *(){
-//   // resolve multiple promises in parallel
-//   var a = Promise.resolve(1);
-//   var b = Promise.resolve(2);
-//   var c = Promise.resolve(3);
-//   var res = yield [a, b, c];
-//   console.log(res);
-//   // => [1, 2, 3]
-// }).catch(onerror);
+co(function *(){
+  // resolve multiple promises in parallel
+  var a = new Promise(function(resolve){
+      
+  });
+  var b = Promise.resolve(2);
+  var c = Promise.resolve(3);
+  var res = yield [a, b, c];
+  //throw new Error("error");
+  console.log(res);
+  // => [1, 2, 3]
+}).catch(onerror);
 
 // errors can be try/catched
-co(function* () {
-    // try {
-    //     yield Promise.reject(new Error('boom'));
-    // } catch (err) {
-    //     console.log('inner err')
-    //     console.error(err.message); // "boom"
-    // }
-    yield Promise.reject(new Error('boom'));
-}).catch(onerror);
+// co(function* () {
+//     try {
+//         yield Promise.reject(new Error('boom'));
+//     } catch (err) {
+//         console.log('inner err')
+//         console.error(err.message); // "boom"
+//     }
+//     yield Promise.reject(new Error('boom'));
+// }).catch(onerror);
 
 function onerror(err) {
     // log any uncaught errors
